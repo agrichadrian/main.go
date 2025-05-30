6a7a535a6a3f3114463d38ddb90d4593696cc6b8
Adrian <mrandrei60@gmail.com>
2025-05-30 08:17:11 +0300

    .

---
+++ Lesson2/lesson2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Adrian"
+	age := 24
+	var height float64 = 1.87
+	isHappy := true
+
+	fmt.Println("Imia:", name)
+	fmt.Println("Vozast:", age)
+	fmt.Println("Rost:", height)
+	fmt.Println("Nastroenie:", isHappy)
+}

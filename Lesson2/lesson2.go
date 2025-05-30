6f1c37b3eee0be36cb7950bbe9f9113baa9229b2
Adrian <mrandrei60@gmail.com>
2025-05-30 10:50:57 +0300

    Update lesson2.go

--- Lesson2/lesson2.go
+++ Lesson2/lesson2.go
@@ -7,9 +7,33 @@ func main() {
 	age := 24
 	var height float64 = 1.87
 	isHappy := true
+	weight := 80
 
-	fmt.Println("Imia:", name)
-	fmt.Println("Vozast:", age)
-	fmt.Println("Rost:", height)
-	fmt.Println("Nastroenie:", isHappy)
+	fmt.Println("Имя:", name)
+	fmt.Println("Возраст:", age)
+	fmt.Println("Рост:", height)
+	fmt.Println("Счастлив?:", isHappy)
+	fmt.Println("Вес:", weight)
+
+	if age < 18 {
+		fmt.Println("Вы несовершеннолетний.")
+	} else if age < 60 {
+		fmt.Println("Вы взрослый человек.")
+	} else {
+		fmt.Println("Вы пенсионер.")
+	}
+
+	if isHappy {
+		fmt.Println("Настроение отличное!")
+	} else {
+		fmt.Println("Держись, всё наладится.")
+	}
+
+	if weight <= 80 {
+		if height > 1.8 {
+			fmt.Println("Ты в хорошей форме!")
+		}
+	} else {
+		fmt.Println("Над физической формой можно поработать :)")
+	}
 }
